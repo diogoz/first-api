959dea0dc63845b83b6f54bf8207b73e9d50c9e3
Diogo Sousa <dhesousa77@gmail.com>
2023-06-15 20:48:29 -0300

    Editar e deletar usuario

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -6,7 +6,7 @@
 // update  (fazer alguma mudança no usuario)
 // delete (remover o usuario)
 
-const users = require("../mocks/users");
+let users = require("../mocks/users");
 
 module.exports = {
   listUsers(request, response) {
@@ -42,5 +42,36 @@ module.exports = {
     users.push(newUser);
 
     response.send(200, newUser);
+  },
+  updateUser(request, response) {
+    let { id } = request.params;
+    const { name } = request.body;
+
+    id = Number(id);
+
+    const userExist = users.find((user) => user.id === id);
+
+    if (!userExist) {
+      return response.send(400, { error: "User not found" });
+    }
+
+    users = users.map((user) => {
+      if (user.id === id) {
+        return {
+          ...user,
+          name,
+        };
+      }
+      return user;
+    });
+    response.send(200, { id, name });
+  },
+  deleteUser(request, response) {
+    let { id } = request.params;
+
+    id = Number(id);
+
+    users = users.filter((user) => user.id !== id);
+    response.send(200, { deleted: true });
   },
 };
